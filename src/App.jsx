49047976bc03bbf937dfd4cf3cf3e3e81ac35c14
Aqil321821 <aqilrao321821@gmail.com>
2025-02-13 18:49:49 +0500

    section 3 basics done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-
+import store from './store/configureStore';
+import { addTask, removeTask, fetchTodo } from './store/tasks';
 function App() {
+  store.dispatch(addTask('Task 1'));
+  store.dispatch(removeTask(1));
+  store.dispatch(fetchTodo());
 
   return (
     <>
       <h1>hello</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
